13cb1044737095659b31283eece978061dc721ac
Gyeonghun Park <gp112795@gmail.com>
2022-01-06 22:37:01 +0900

    Reusable Configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+
+const { withFrameworkConfig } = require('./framework/common/config');
+
+module.exports = withFrameworkConfig({
+  framework: {
+    name: 'shopify_local',
+  },
+  i18n: {
+    locales: ['en-US', 'ko'],
+    defaultLocale: 'en-US',
+  },
   reactStrictMode: true,
-}
+});
